978b98015894d1866a5bd1f73dd51eb798d770bf
Eivind Hjertnes <eivind.hjertnes@knowit.no>
2024-09-18 08:04:44 +0200

    Simplify & prettify
    - Remove chart & javascript
    - Remove args & replace with stdin
    - Prettier output

--- Dockerfile
+++ Dockerfile
@@ -6,10 +6,10 @@ COPY templates/* /app/templates/
 COPY entrypoint.sh /app/
 COPY parse-sast.py /app/
 COPY requirements.txt /app/requirements.txt
-
+RUN apk add jq
 RUN pip install --no-cache-dir -r /app/requirements.txt \
     && apk add --no-cache curl
 
 WORKDIR /reports
 
-ENTRYPOINT [ "/app/entrypoint.sh" ]
+#ENTRYPOINT [ "/app/entrypoint.sh" ]
--- parse-sast.py
+++ parse-sast.py
@@ -31,54 +31,32 @@ def countSeverities(vulnerabilities):
 
     return frequencies
 
-parser = argparse.ArgumentParser(description='Parse a GitLab SAST report to HTML')
-parser.add_argument('files', metavar='files', nargs='+',
-                    help='The files that should be converted to HTML.')
-parser.add_argument('--only-severities', type=str, required=False,
-                    help='A comma delimited list of the vulnerabilities to keep (defaults to all)')
-parser.add_argument('--jsonpath-filter', type=str, required=False,
-                    help='Provide a custom jsonpath filter to apply to all JSON files')
-parser.add_argument('--no-verify-version', dest='verify_version', action='store_false',
-                    help='Disable verification of the report version before attempting to parse it.' )
-parser.set_defaults(verify_version=True)
-args = parser.parse_args()
-
-if args.jsonpath_filter is not None:
-    try:
-        jsonpath_expr = parse(args.jsonpath_filter)
-    except:
-        print('Invalid jsonpath filter provided')
-        sys.exit(1)
-# Build a filter based on severities
-elif args.only_severities is not None:
-    severities = args.only_severities.split(',')
-    filters = list()
-    for severity in severities:
-        filters.append("($.vulnerabilities[?(@.severity == '" + severity + "')])")
-    filter = ' | '.join(filters)
-    jsonpath_expr = parse(filter)
-else:
+if __name__ == "__main__":
     jsonpath_expr = parse("$.vulnerabilities[*]")
+    data = None
+    raw_input = "".join(sys.stdin.readlines())
+    try:
+        data = json.loads(raw_input)
+    except json.JSONDecodeError:
+        if os.path.exists(raw_input) and os.path.isfile(raw_input):
+            with open(raw_input, "r") as f:
+                data = json.load(f)
+        else:
+            if not os.path.exists(raw_input):
+                print(f"Path doesn't exist {raw_input}")
+            elif not os.path.isfile(raw_input):
+                print(f"Please enter a valid JSON file {raw_input}")
 
-vulnerabilities = list()
+    if data is not None:
+        vulnerabilities = [vuln.value for vuln in jsonpath_expr.find(data)]
+        frequencies=countSeverities(vulnerabilities)
 
-# Iterate over all the JSON files provided
-for json_file in args.files:
-    with open(json_file) as f:
-        data = json.load(f)
-
-        if args.verify_version and Version(data['version']) >= Version("16.0"):
-            print('We don\'t know how to parse this version of SAST report')
-            sys.exit(1)
-
-        vulns = jsonpath_expr.find(data)
-        for vuln in vulns:
-            vulnerabilities.append(vuln.value)
-
-frequencies=countSeverities(vulnerabilities)
-
-project_dir = os.path.dirname(__file__)
-env = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.join(project_dir, 'templates')))
-template = env.get_template('vulnerability_report.html')
-rendered = template.render(vulnerabilities=vulnerabilities, frequencies=countSeverities(vulnerabilities))
-print(rendered)
+        project_dir = os.path.dirname(__file__)
+        env = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.join(project_dir, 'templates')))
+        template = env.get_template('vulnerability_report.html')
+        rendered = template.render(vulnerabilities=vulnerabilities, frequencies=countSeverities(vulnerabilities))
+        print(rendered)
+        sys.exit(0)
+    else:
+        print("No data or file path provided on stdin")
+        sys.exit(1)
